ad5e150ab26158bb6289128e86144fc61531e920
jbs2507 <julibesa25@gmail.com>
2026-04-07 16:41:28 -0500

    guardar antes de deploy

--- src/features/layout/components/Content.jsx
+++ src/features/layout/components/Content.jsx
@@ -1,42 +1,43 @@
 import React from 'react'
 
 export const Content = () => {
+  const baseUrl = import.meta.env.BASE_URL;
   const productos = [
     {
       id: 1,
       nombre: "Chocobarra",
       precio: 2.500,
-      img: "/img/Chocobarra.png",
+      img: `${baseUrl}img/Chocobarra.png`,
     },
     {
       id: 2,
       nombre: "Gomitas fresa mix",
       precio: 3.000,
-      img: "/img/Gomita-fresa.png",
+      img: `${baseUrl}img/Gomita-fresa.png`,
     },
     {
       id: 3,
       nombre: "Galleta ChocoChips",
       precio: 1.800,
-      img: "/img/Galleta-chocoChips.png",
+      img: `${baseUrl}img/Galleta-chocoChips.png`,
     },
     {
       id: 4,
       nombre: "Cupcake Vainilla",
       precio: 4.000,
-      img: "/img/Cupcake-vainilla.png",
+      img: `${baseUrl}img/Cupcake-vainilla.png`,
     },
     {
       id: 5,
       nombre: "Paleta arcoíris",
       precio: 1.500,
-      img: "/img/Paleta-arcoiris.png",
+      img: `${baseUrl}img/Paleta-arcoiris.png`,
     },
     {
       id: 6,
       nombre: "Mini donitas",
       precio: 3.500,
-      img: "/img/Mini-donitas.jpg",
+      img: `${baseUrl}img/Mini-donitas.jpg`,
     },
   ];
 
