ed84feccadc2fe600cb53e6002ef729bc9849489
jbs2507 <julibesa25@gmail.com>
2026-04-07 16:42:15 -0500

    guardar antes de deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,26 @@
-// src/App.jsx
-import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+// import { useState } from 'react'
+// import './src/shared/styles/App.css'
 
-// Componentes de layout
-import { Header } from "./features/layout/components/Header";
-import { Content } from "./features/layout/components/Content";
-import { Footer } from "./features/layout/components/Footer";
-import Props from "./features/layout/components/Props";
+import { Header } from "./features/layout/components/Header"
+import { Content } from "./features/layout/components/Content"
+import { Footer } from "./features/layout/components/Footer"
+import Props from "./features/layout/components/Props"
+
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
+
+
   return (
     <HashRouter>
-      {/* Header */}
-      <Header />
-
-      {/* Sección principal: imágenes que aparecen al abrir el link */}
-      <div style={{ textAlign: "center", margin: "20px 0" }}>
-        <h1>Mis dulces favoritos</h1>
-        <img
-          src="/React/img/paleta-arcoiris.png"
-          alt="Paleta arcoiris"
-          style={{ width: "200px", margin: "10px" }}
-        />
-        <img
-          src="/React/img/cupcake-vainilla.png"
-          alt="Cupcake vainilla"
-          style={{ width: "200px", margin: "10px" }}
-        />
-        <img
-          src="/React/img/mini-donitas.jpg"
-          alt="Mini donitas"
-          style={{ width: "200px", margin: "10px" }}
-        />
-        <img
-          src="/React/img/gomita-fresa-mix.png"
-          alt="Gomita fresa"
-          style={{ width: "200px", margin: "10px" }}
-        />
-        <img
-          src="/React/img/galleta-chocochips.png"
-          alt="Galleta ChocoChips"
-          style={{ width: "200px", margin: "10px" }}
-        />
-        <img
-          src="/React/img/chocobarra.png"
-          alt="Chocobarra"
-          style={{ width: "200px", margin: "10px" }}
-        />
-      </div>
-
-      {/* Rutas */}
+      <Header></Header>
       <Routes>
-        <Route path="/" element={<Content />} />
-        <Route path="/props" element={<Props />} />
+        <Route path="/" element={<Content></Content >}> </Route>
+        <Route path="/props" element={<Props></Props>}></Route>
       </Routes>
-
-      {/* Footer */}
-      <Footer />
+      <Footer></Footer>
     </HashRouter>
-  );
+  )
 }
 
-export default App;
+export default App
